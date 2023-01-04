cc21d6cfb90003c592118bd993f0b0e3f51cd7d2
김하영(**2113) / 서비스개발담당 WEB개발팀 <202113@ssg.com>
2023-01-04 17:03:56 +0900

    커뮤니티 생성 페이지 기능 만들기

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -1,12 +1,34 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 import InputGroup from "../../components/InputGroup";
 
 const SubCreate = () => {
+	const [name, setName] = useState("");
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [errors, setErrors] = useState<any>({});
+	let router = useRouter();
+
+	const handleSubmit = async (event: FormEvent) => {
+		event.preventDefault();
+
+		try {
+			const res = await axios.post("/subs", { name, title, description });
+
+			router.push(`/r/${res.data.name}`);
+		} catch (error: any) {
+			console.log(error);
+			setErrors(error.response.data);
+		}
+	};
+
 	return (
 		<div className="flex flex-col justify-center pt-16">
 			<div className="w-10/12 p-4 mx-auto bg-white rounded md:w-96">
 				<h1 className="mb-2 text-lg font-medium">커뮤니티 만들기</h1>
 				<hr />
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div className="my-6">
 						<p className="font-medium">Name</p>
 						<p className="mb-2 text-xs text-gray-400">커뮤니티 이름은 변경할 수 없습니다.</p>
