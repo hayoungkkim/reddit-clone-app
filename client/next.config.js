0690b8007c863d84ea00c87a2f889f6f456fa767
김하영(**2113) / 서비스개발담당 WEB개발팀 <202113@ssg.com>
2023-01-05 16:16:11 +0900

    커뮤니티 리스트 생성하기, Type 지정하기

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["www.gravatar.com", "localhost", "ec2-43-200-8-24.ap-northeast-2.compute.amazonaws.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
