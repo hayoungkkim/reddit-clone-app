0f8cdbad6c8a22a7afd6589a26ed7d57d35767fe
김하영(**2113) / 서비스개발담당 WEB개발팀 <202113@ssg.com>
2023-01-06 13:59:30 +0900

    포스트와 댓글을 위한 Vote (좋아요 싫어요) 기능 구현하기

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import classNames from "classnames";
 import dayjs from "dayjs";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -12,7 +13,7 @@ const PostPage = () => {
 	const { identifier, sub, slug } = router.query;
 	const { authenticated, user } = useAuthState();
 	const [newComment, setNewComment] = useState("");
-	const { data: post, error } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
+	const { data: post, error, mutate: postMutate } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
 	const { data: comments, mutate: commentMutate } = useSWR<Comment[]>(identifier && slug ? `/posts/${identifier}/${slug}/comments` : null);
 
 	const handleSubmit = async (e: FormEvent) => {
@@ -31,6 +32,26 @@ const PostPage = () => {
 			console.log(error);
 		}
 	};
+
+	const vote = async (value: number, comment?: Comment) => {
+		if (!authenticated) router.push("/login");
+
+		// 이미 클릭 한 vote 버튼을 눌렀을 시에는 reset
+		if ((!comment && value === post?.userVote) || (comment && comment.userVote === value)) {
+			value = 0;
+		}
+
+		try {
+			await axios.post("/votes", {
+				identifier,
+				slug,
+				commentIdentifier: comment?.identifier,
+				value,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return (
 		<div className="flex max-w-5xl px-4 pt-5 mx-auto">
 			<div className="w-full md:mr-3 md:w-8/12">
@@ -38,6 +59,26 @@ const PostPage = () => {
 					{post && (
 						<>
 							<div className="flex">
+								{/* 좋아요 싫어요 기능 부분 */}
+								<div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+									{/* 좋아요 */}
+									<div className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500" onClick={() => vote(1)}>
+										<i
+											className={classNames("fas fa-arrow-up", {
+												"text-red-500": post.userVote === 1,
+											})}
+										></i>
+									</div>
+									<p className="text-xs font-bold">{post.voteScore}</p>
+									{/* 싫어요 */}
+									<div className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500" onClick={() => vote(-1)}>
+										<i
+											className={classNames("fas fa-arrow-down", {
+												"text-blue-500": post.userVote === -1,
+											})}
+										></i>
+									</div>
+								</div>
 								<div className="py-2 pr-2">
 									<div className="flex items-center">
 										<p className="text-xs text-gray-400">
@@ -94,6 +135,26 @@ const PostPage = () => {
 							{/* 댓글 리스트 부분 */}
 							{comments?.map((comment) => (
 								<div className="flex" key={comment.identifier}>
+									{/* 좋아요 싫어요 기능 부분 */}
+									<div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+										{/* 좋아요 */}
+										<div className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500" onClick={() => vote(1, comment)}>
+											<i
+												className={classNames("fas fa-arrow-up", {
+													"text-red-500": comment.userVote === 1,
+												})}
+											></i>
+										</div>
+										<p className="text-xs font-bold">{comment.voteScore}</p>
+										{/* 싫어요 */}
+										<div className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500" onClick={() => vote(-1, comment)}>
+											<i
+												className={classNames("fas fa-arrow-down", {
+													"text-blue-500": comment.userVote === -1,
+												})}
+											></i>
+										</div>
+									</div>
 									<div className="py-2 pr-2">
 										<p className="mb-1 text-xs leading-none">
 											<Link href={`/u/${comment.username}`} className="mr-1 font-bold hover:underline">
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from "./data-source";
 import authRoutes from "./routes/auth";
 import subRoutes from "./routes/subs";
 import postRoutes from "./routes/posts";
+import voteRoutes from "./routes/votes";
 
 import cors from "cors";
 import dotenv from "dotenv";
@@ -28,6 +29,7 @@ app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);
 app.use("/api/subs", subRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/votes", voteRoutes);
 
 // static 파일이 public 폴더 안에 있고 브라우저로 접근할 때 제공 할 수 있게 해줌
 app.use(express.static("public"));
