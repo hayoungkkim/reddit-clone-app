7aa18b960f8c83a3e84e203b5817eb82b88083e8
김하영(**2113) / 서비스개발담당 WEB개발팀 <202113@ssg.com>
2023-01-06 09:41:24 +0900

    이미지 업로드 하기

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["www.gravatar.com", "localhost", "ec2-43-200-8-24.ap-northeast-2.compute.amazonaws.com"],
+		domains: ["www.gravatar.com", "localhost"],
 	},
 };
 
