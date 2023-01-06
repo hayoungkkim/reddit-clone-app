d47a08c1bc16a06e2471c8ab1531493f2cfdd6d5
김하영(**2113) / 서비스개발담당 WEB개발팀 <202113@ssg.com>
2023-01-06 16:57:37 +0900

    Vote 함수 생성하기 및 router.pathname 사용

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,46 +1,68 @@
+import axios from "axios";
 import dayjs from "dayjs";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { useAuthState } from "../context/auth";
 import { Post } from "../types";
 
 interface PostCardProps {
 	post: Post;
+	subMutate?: () => void;
+	mutate?: () => void;
 }
 
-const PostCard = ({ post: { identifier, slug, title, body, subName, createdAt, voteScore, userVote, commentCount, url, username, sub } }: PostCardProps) => {
+const PostCard = ({ post: { identifier, slug, title, body, subName, createdAt, voteScore, userVote, commentCount, url, username, sub }, mutate, subMutate }: PostCardProps) => {
+	console.log(userVote);
+	const router = useRouter();
+	const isInSubPage = router.pathname === "/r/[sub]";
+
+	const { authenticated } = useAuthState();
+
+	const vote = async (value: number) => {
+		if (!authenticated) router.push("/login");
+
+		if (value === userVote) value = 0;
+
+		try {
+			await axios.post("/votes", { identifier, slug, value });
+			if (mutate) mutate();
+			if (subMutate) subMutate();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="flex mb-4 bg-white rounded" id={identifier}>
 			{/* 좋아요 싫어요 기능 부분 */}
 			<div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
 				{/* 좋아요 */}
-				<div
-					className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-					// onClick={() => vote(1, comment)}
-				>
+				<div className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500" onClick={() => vote(1)}>
 					{userVote === 1 ? <FaArrowUp className="text-red-500" /> : <FaArrowUp />}
 				</div>
 				<p className="text-xs font-bold">{voteScore}</p>
 				{/* 싫어요 */}
-				<div
-					className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-					// onClick={() => vote(-1, comment)}
-				>
+				<div className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500" onClick={() => vote(-1)}>
 					{userVote === -1 ? <FaArrowDown className="text-blue-500" /> : <FaArrowDown />}
 				</div>
 			</div>
 			{/* 포스트 데이터 부분 */}
 			<div className="w-full p-2">
 				<div className="flex items-center">
-					{/* <div className="flex items-center">
-						<Link href={`/r/${subName}`}>
-							<Image src={sub!.imageUrl} alt="sub" className="w-6 h-6 mr-1 rounded-full cursor-pointer" />
-						</Link>
-						<Link href={`/r/${subName}`} className="text-xs font-bold cursor-pointer hover:underline">
-							/r/{subName}
-						</Link>
-						<span className="mx-1 text-xs text-gray-400">•</span>
-					</div> */}
+					{!isInSubPage && (
+						<div className="flex items-center">
+							<Link href={`/r/${subName}`}>
+								<Image src={sub!.imageUrl} alt="sub" className="w-6 h-6 mr-1 rounded-full cursor-pointer" />
+							</Link>
+							<Link href={`/r/${subName}`} className="text-xs font-bold cursor-pointer hover:underline">
+								/r/{subName}
+							</Link>
+							<span className="mx-1 text-xs text-gray-400">•</span>
+						</div>
+					)}
 					<p className="text-xs text-gray-400">
 						Posted by <i className="fas fa-abacus"></i>
 						<Link href={`/u/${username}`} className="mx-1 hover:underline">
@@ -51,7 +73,9 @@ const PostCard = ({ post: { identifier, slug, title, body, subName, createdAt, v
 						</Link>
 					</p>
 				</div>
-				<h1 className="my-1 text-xl font-medium">{title}</h1>
+				<Link href={url} className="my-1 text-lg font-medium">
+					{title}
+				</Link>
 				{body && <p className="my-3 text-sm">{body}</p>}
 				<div className="flex">
 					<Link href={url}>
